f3047d1634692bb47cc637f0cbdc336822b478eb
Yifan Hu <224434986+yhu1729@users.noreply.github.com>
2025-09-12 18:11:28 -0400

    feat: update style guide

--- format.sh
+++ format.sh
@@ -8,6 +8,6 @@ TARGET_TEX=$(find ./ -type f -name "*.tex")
 $LATEXINDENT $LATEXINDENT_FLAGS $TARGET_TEX
 
 BIBTEXTIDY=bibtex-tidy
-BIBTEXTIDY_FLAGS="--modify --omit=abstract,shortjournal,file,keywords,langid,note,pagetotal,rights,urldate --curly --numeric --no-align --sort=key --duplicates=key,doi --strip-enclosing-braces --drop-all-caps --no-escape --sort-fields=doi,isbn,author,editor,translator,year,date,title,shorttitle,edition,journal,journaltitle,issn,booktitle,series,pages,publisher,location,volume,number,url --strip-comments --trailing-commas --remove-empty-fields --wrap=79"
+BIBTEXTIDY_FLAGS="--modify --omit=abstract,shortjournal,file,keywords,language,langid,note,pagetotal,month,rights,urldate --curly --numeric --no-align --sort=key --duplicates=key,doi --strip-enclosing-braces --drop-all-caps --no-escape --sort-fields=doi,isbn,author,editor,translator,year,date,title,shorttitle,edition,journal,journaltitle,issn,booktitle,series,pages,publisher,location,volume,number,url --strip-comments --trailing-commas --remove-empty-fields --wrap=79"
 TARGET_BIB=$(find ./ -type f -name "*.bib")
 $BIBTEXTIDY $BIBTEXTIDY_FLAGS $TARGET_BIB
